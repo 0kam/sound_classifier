36323d9ec25d1fac85a11436f9653dae4eb1d06c
0kam <okamoto.ryotaro@nies.go.jp>
2023-01-25 17:21:33 +0900

    trained yamnet model

--- sound_classifier/yamnet_vn/train.py
+++ sound_classifier/yamnet_vn/train.py
@@ -2,7 +2,7 @@ from sound_classifier.yamnet import YAMNet
 from sound_classifier.yamnet_vn import params
 from tensorflow_addons.optimizers import RectifiedAdam
 from tensorflow import optimizers as optim
-from audiomentations import Compose, AirAbsorption, AddBackgroundNoise, TanhDistortion, PitchShift, AddGaussianNoise
+from audiomentations import Compose, AirAbsorption, AddBackgroundNoise, TanhDistortion, PitchShift, AddGaussianNoise, Gain
 
 yamnet = YAMNet("sound_classifier.yamnet_vn.params")
 yamnet.load_weights(
@@ -11,10 +11,11 @@ yamnet.load_weights(
 )
 
 augs = Compose([
+    Gain(min_gain_in_db = -30, max_gain_in_db=0, p = 1.0),
     AirAbsorption(p = 0.75),
     TanhDistortion(),
     PitchShift(),
-    AddGaussianNoise(p = 1.0)
+    AddGaussianNoise(p = 0.75)
 ])
 
 ds = yamnet.dataset(
@@ -31,7 +32,8 @@ ds = yamnet.dataset(
 train_ds = ds.set_mode("train")
 val_ds = ds.set_mode("val")
 
-train_ds.augmentations = None
+train_ds.normalize = False
+val_ds.normalize = False
 
 optim_top = RectifiedAdam(learning_rate = 5e-4)
 yamnet.train(30, train_ds, val_ds, optim_top, fine_tune=False, workers=18)
@@ -39,11 +41,11 @@ res1 = yamnet.evaluate(val_ds, 0.5)
 optim_finetune = RectifiedAdam(learning_rate = 5e-5)
 yamnet.train(30, train_ds, val_ds, optim_finetune, fine_tune = True, n_layers = 3, workers=18)
 res2 = yamnet.evaluate(val_ds, 0.5)
-yamnet.save_weights("sound_classifier/yamnet_vn/finetune.h5")
 
 yamnet.save_weights("sound_classifier/yamnet_vn/finetune.h5", model_base = False)
 yamnet.save_weights("sound_classifier/yamnet_vn/finetune_base.h5", model_base = True)
 
 yamnet2 = YAMNet("sound_classifier.yamnet_vn.params")
-yamnet2.train(30, train_ds, val_ds, optim_finetune, fine_tune = True, n_layers = 14, workers=18)
+optim_scratch = RectifiedAdam(learning_rate = 1e-3)
+yamnet2.train(30, train_ds, val_ds, optim_scratch, fine_tune = True, n_layers = 14, workers=18)
 res3 = yamnet2.evaluate(val_ds, 0.5)
